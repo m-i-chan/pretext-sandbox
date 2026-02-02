c520b08f4a3e6447e8ffb65738325bab84cee4a2
m-i-chan <michan1910@gmail.com>
2026-02-02 11:51:50 -0500

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -6,8 +6,8 @@ var ptx_lunr_docs = [
   "url": "cover-sheet.html",
   "type": "Chapter",
   "number": "",
-  "title": "Cover Sheet",
-  "body": " Cover Sheet   This is a test page for playing around with PreTeXt content.   "
+  "title": "About",
+  "body": " About  Marc is a senior at Framingham State University pursuing a degree in computer science. His hobbies include video games, hiking, and working on his Miata.  This site is a sandbox for learning PreTeXt.  "
 },
 {
   "id": "linear-description",
@@ -16,16 +16,16 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Lesson Description",
-  "body": " Lesson Description  This section contains a lesson from Prof. Atchison's MATH123 course. It was originally written in LaTeX and has been converted to PreTeXt as an exercise.   Objective:  Solve, graph, and give interval notation for the solution to a linear inequality. Create a sign diagram to identify those intervals where a linear expression is positive or negative.    Students will be able to:    Solve a linear inequality by isolating the variable.  Recognize the need to change the direction of an inequality when multiplying or dividing by a negative value.  Graph a linear inequality on a one-dimensional axis.  Express solutions using interval notation.      Prerequisite Knowledge:    Apply the distributive property.  Verify the accuracy of a solution to an inequality by checking     "
+  "body": " Lesson Description   Objective:  Solve, graph, and give interval notation for the solution to a linear inequality. Create a sign diagram to identify those intervals where a linear expression is positive or negative.    Students will be able to:    Solve a linear inequality by isolating the variable.  Recognize the need to change the direction of an inequality when multiplying or dividing by a negative value.  Graph a linear inequality on a one-dimensional axis.  Express solutions using interval notation.      Prerequisite Knowledge:    Apply the distributive property.  Verify the accuracy of a solution to an inequality by checking.     "
 },
 {
-  "id": "sec-Author-Profile",
+  "id": "linear-lesson",
   "level": "1",
-  "url": "sec-Author-Profile.html",
+  "url": "linear-lesson.html",
   "type": "Section",
   "number": "",
-  "title": "Author Profile",
-  "body": " Author Profile  Marc is a senior attending Framingham State University and is pursuing a bachelor's in computer science. His hobbies include computers and working on his Miata.  "
+  "title": "Lesson",
+  "body": " Lesson   Motivating Example  Solve the linear equality.   Our solution can be expressed as follows:   Verbally:  The set of all values of that are greater than or equal to (at least)     Inequality:       Interval:       Graphically (number line):     Note: A closed (shaded) circle at is also acceptable in place of a bracket.      "
 }
 ]
 
